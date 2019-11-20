02d2e61d7bd0b028a01fc40958f3add960a0f8c1
Sabrina <sabrina.muniz@icloud.com>
2019-11-20 13:50:19 -0500

    added file structure and npm packages

---
+++ friendFinder/server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
