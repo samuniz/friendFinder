a4a82edb2f2387d76563e6515d51f393b805499e
Sabrina <sabrina.muniz@icloud.com>
2019-11-22 08:58:15 -0500

    added api and html routes

--- friendFinder/app/routing/htmlRoutes.js
+++ friendFinder/app/routing/htmlRoutes.js
@@ -6,7 +6,7 @@ module.exports = function(app){
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  app.get("/", function(req, res) {
+  app.get('/survey', function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 }
--- friendFinder/server.js
+++ friendFinder/server.js
@@ -26,9 +26,8 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-
-// require("./app/routing/apiRoutes")(app);
-// require("./app/routing/htmlRoutes")(app);
+require('./app/routing/apiRoutes') (app); 
+require('./app/routing/htmlRoutes') (app);
 
 // // =============================================================================
 // // LISTENER
