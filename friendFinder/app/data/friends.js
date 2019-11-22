209ed4995fa3d123e42c7f3166f5b158abab75f7
Sabrina <sabrina.muniz@icloud.com>
2019-11-22 08:29:01 -0500

    created html pages and add data

---
+++ friendFinder/app/data/friends.js
@@ -0,0 +1,86 @@
+var friendsArray= [
+  {
+  "name":"Charlie",
+  "photo":"https://i.pinimg.com/originals/48/2e/9e/482e9e214cb937db206e4bb6e8303cbc.jpg",
+  "scores":[
+      5,
+      1,
+      5,
+      3,
+      5,
+      3,
+      1,
+      5,
+      1,
+      5
+    ]
+},
+{
+  "name": "Deandra",
+  "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqxTu8oLi7ix2YamYnLPAyX-PLZYba4Mm9QbrtOQrtj2rp2uaBTg&s",
+  "scores":[
+    5,
+    1,
+    5,
+    3,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+  ]
+},
+{
+  "name": "Mac",
+  "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNyAG04ALcwZeyp8K2I7LQ8u_qBOvalOVxxxJj5pmUQ8THOv6_ZQ&s",
+  "scores":[
+    5,
+    1,
+    5,
+    3,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+  ]
+},
+
+{
+  "name": "Dennis",
+  "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSn39S0GrUJMnPAnK71xHKYEHsZ3tt1wQ-MUzoQgmcHrULRU7j&s",
+  "scores":[
+    5,
+    1,
+    5,
+    3,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+  ]
+},
+{
+  "name": "Frank",
+  "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0vm9HsW7HZyzSn3nqVLZCajqv1KLajTSpiBSLvmL3f4RteY8&s",
+  "scores":[
+    5,
+    1,
+    5,
+    3,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+  ]
+}
+
+
+]
+module.exports = friendsArray;
--- friendFinder/server.js
+++ friendFinder/server.js
@@ -1,11 +1,40 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 var express = require("express");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
-var PORT = 3000;
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+// // =============================================================================
+// // LISTENER
+// // The below code effectively "starts" our server
+// // =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
