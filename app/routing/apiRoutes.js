d128ee3c204b8ceacb53601018808efe08f0158d
Sabrina <sabrina.muniz@icloud.com>
2019-11-23 17:50:10 -0500

    added post route to grab user input and on click function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,5 +3,10 @@ var friendData = require('../data/friends');
 module.exports = function (app){
   app.get('/api/friends',  function(req, res){
     res.json(friendData); 
-  })
+  });
+
+  app.post('/api/friends', function(req, res){
+    res.json(true); 
+  }); 
+
 }
