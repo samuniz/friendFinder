0ab441b402053014bfb4cfcfbd56954e0afe0683
Sabrina <sabrina.muniz@icloud.com>
2019-11-24 14:51:33 -0500

    modfied the function that compare the smallest value in the array and reference to my friendData Array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,37 +7,34 @@ module.exports = function(app){
 
   app.post('/api/friends', function(req, res){
     //variable that grabs my user score
-        var userScores = req.body; 
+        var userData = req.body; 
     //variable that store the total of the user and friend score diference
         var totalArray = []
 
 // loop thru the friendData lenght
-    for(var i = 0
-      ; i < friendData.length; i ++){
+    for(var i = 0; i < friendData.length; i ++){
       // variable that stores the sum of the differences of the user score and the friend score
-      var difUserData = 0;
-        for(var j=0; j < userScores.length; j ++){
-          difUserData = Math.abs(userScores.scores[j] - friendData[i].scores[j]);
+      var difUserData = 0; 
+        for(var j=0; j < userData.scores.length; j ++){
+          difUserData += Math.abs(parseInt(userData.scores[j]) - friendData[i].scores[j]);
     };
 
     totalArray.push(difUserData); 
-    // console.log(totalArray);  
+    console.log(totalArray);  
   }
-  // Compare the totalArray and see each one is the smaller
-    if(totalArray.length === 0){
-      return -1;
+// This function capture the index of the smalest value in an array 
+  function indexOfSmallest(a) {
+    var lowest = 0;
+    for (var i = 1; i < a.length; i++) {
+     if (a[i] < a[lowest]) lowest = i;
     }
-    var bestMatch = 0;
-    var lowesstScore = totalArray[0];
-    for (var i = 1; i < totalArray.length; i++) {
-      if (totalArray[i] < lowesstScore) {
-        bestMatch = i; 
-        lowesstScore = totalArray[i];
-        
-  }
-}
-console.log(bestMatch); 
-// res.json(bestMatch); 
+    return lowest;
+   }
+// this variable run the index of smallest function referencing the total score array
+   var bestMatchIndex = (indexOfSmallest(totalArray));
+   console.log(friendData[bestMatchIndex]); 
+   res.json(friendData[bestMatchIndex]); 
+   
 }); 
 
 }
