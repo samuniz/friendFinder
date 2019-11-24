9ee0e19f62c086a8838b2d1e73d8fe7878910f5a
Sabrina <sabrina.muniz@icloud.com>
2019-11-23 20:24:45 -0500

    added logic comparrison on post request

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,23 @@ module.exports = function (app){
   });
 
   app.post('/api/friends', function(req, res){
-    res.json(true); 
+    var userScores = req.body.scores; 
+    var totalArray= []; 
+    var match = 0; 
+    // var friendScore = friendData[i].score[j]; 
+    // console.log(friendData); 
+    for(var i = 0; i < friendData.length; i ++){
+       var matchSubtraction = 0
+      for(var j=0; j < userScores.length; j ++){
+        matchSubtraction += (Math.abs(friendData[i].scores[j] - parseInt(userScores[j])));
+      }
+      totalArray.push(matchSubtraction); 
+    }
+     console.log(totalArray); 
+
+     res.json(friendData[match])
+     friendData.push(req.body);
+    //  console.log("new: " + JSON.stringify(req.body));
   }); 
 
 }
